3a2a75e921d4797663acbb9d02a095899ad7fb5c
xh <xiaoyi0527@sina.cn>
2017-11-16 17:31:46 +0800

    update form submit info ond validation

--- js/main.js
+++ js/main.js
@@ -254,7 +254,7 @@
 		      error: function() {
 
 		      	sLoader.fadeOut(); 
-		      	$('#message-warning').html("Something went wrong. Please try again.");
+		      	$('#message-warning').html("出现错误. 请重新再试.");
 		         $('#message-warning').fadeIn();
 
 		      }
